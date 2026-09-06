dca1f032d9ddec75d855eff665f1ad4a58e6b10e
Abdurahman AYDIN <hariysu@gmail.com>
2026-09-06 16:16:07 +0300

    Complete Guess The Flag application

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Abdurahman on 4.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
